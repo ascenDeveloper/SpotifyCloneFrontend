383ea1aa558cdb6d0455918e31a7cf2d4ac7e6ca
Ascended300 <eduardofelix014@gmail.com>
2022-05-05 13:22:25 -0300

    Avances de spotify.jsx inicio

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Body from './Body';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 function Spotify() {
   return (
     <Container>
         <div className="spotify__body">
+            <Sidebar></Sidebar>
             <div className="body">
-                <div className="body__contents"></div>
+                <Navbar></Navbar>
+                <div className="body__contents">
+                    <Body></Body>
+                </div>
             </div>
         </div>
         <div className="spotify__footer"></div>
@@ -17,5 +24,7 @@ function Spotify() {
 export default Spotify;
 
 const Container = styled.div`
-
+    max-width: 100vw;
+    max-height: 100vh;
+    overflow: hidden;
 `;
