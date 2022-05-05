e178d342ed79015ba6937371be63686de4662d1e
Ascended300 <eduardofelix014@gmail.com>
2022-05-05 16:10:41 -0300

    Añadido y modificado el footer sidebar, añadidos a spotify.jsx con styled components

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Body from './Body';
+import Footer from './Footer';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
@@ -16,7 +17,9 @@ function Spotify() {
                 </div>
             </div>
         </div>
-        <div className="spotify__footer"></div>
+        <div className="spotify__footer">
+            <Footer></Footer>
+        </div>
     </Container>
   )
 }
@@ -27,4 +30,19 @@ const Container = styled.div`
     max-width: 100vw;
     max-height: 100vh;
     overflow: hidden;
+    display: grid;
+    grid-template-rows: 85vh 15vh;
+    .spotify__body {
+        display: grid;
+        grid-template-columns: 15vw 85vh;
+        height: 100%;
+        width: 100%;
+        background: linear-gradient(transparent, rgba(0,0,0,1));
+        background-color: rgb(32, 87, 100);
+        .body {
+            height: 100%;
+            width: 100%;
+            overflow: auto;
+        }
+    }
 `;
