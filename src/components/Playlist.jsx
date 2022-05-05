04d53fba1827eb2d6814ff8925504055695ee562
Ascended300 <eduardofelix014@gmail.com>
2022-05-05 20:43:43 -0300

    Formateo de datos de la api, agregada constant e inicio al tratamiento de datos

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, {useEffect} from 'react';
+import { reducerCases } from '../utils/Constants';
 import {useStateProvider} from '../utils/StateProvider';
 
 function Playlist() {
@@ -7,13 +8,17 @@ function Playlist() {
 
     useEffect(()=>{
         const getPlaylistData = async () => {
-            const response = await axios.get('https://api.spotify.com/v1/me', {
+            const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
                 headers: {
                     Authorization:'Bearer '+ token,
                     'Content-Type': 'application/json',
                 },
             });
-            console.log(response);
+            const {items} = response.data;
+            const playlist = items.map(({name , id}) => {
+                return {name, id};
+            })
+            dispatch({type:reducerCases.SET_PLAYLISTS, playlist})
             
         };
         getPlaylistData();
