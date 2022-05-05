41813cb1fac77861da958223ce5fbbc2deba3329
Ascended300 <eduardofelix014@gmail.com>
2022-05-05 18:33:00 -0300

    Inicios de sidebar con hover en los botones + logo de sporify en la parte superior

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import {IoLibrary} from 'react-icons/io5';
+import {MdHomeFilled, MdSearch} from 'react-icons/md';
 
 function Sidebar() {
-  return <Container>Sidebar</Container>
+  return <Container>
+    <div className='top__links'>
+      <div className="logo">
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt="spotify" />
+      </div>
+      <ul>
+        <li>
+          <MdHomeFilled></MdHomeFilled>
+          <span>Home</span>
+        </li>
+        <li>
+          <MdSearch></MdSearch>
+          <span>Search</span>
+        </li>
+        <li>
+          <IoLibrary></IoLibrary>
+          <span>Your Library</span>
+        </li>
+      </ul>
+    </div>
+  </Container>
 }
 
 export default Sidebar;
 
 const Container = styled.div`
   background-color: black;
+  color: #b3b3b3;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  .top__links {
+    display: flex;
+    flex-direction: column;
+    .logo {
+      text-align: center;
+      margin: 1rem 0;
+      img {
+        max-inline-size: 80%;
+        block-size: auto;
+      }
+    }
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      padding: 1rem;
+      li {
+        cursor: pointer;
+        display: flex;
+        gap: 1rem;
+        transition: 0.3 ease-in-out;
+        &:hover {
+          color: white;
+        }
+      }
+    }
+  }
 `;
