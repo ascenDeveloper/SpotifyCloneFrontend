c19596bff4270d2890ef9d4e037fcf6292ed83eb
Ascended300 <eduardofelix014@gmail.com>
2022-05-04 23:45:04 -0300

    Extracción de token en app.js y creación del archivo Constants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Logins from './components/Logins'
 
 function App() {
+   
+  useEffect(() => {
+    const hash = window.location.hash;
+    if(hash) {
+      const token = hash.substring(1).split('&')[0].split('=')[1];
+      console.log(token);
+    }
+  }, [])
   return (
     <div>
       <Logins></Logins>
