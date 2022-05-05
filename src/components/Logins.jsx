b5b8ca0c9fd26b149e948b6374042e5ca925a50a
Ascended300 <eduardofelix014@gmail.com>
2022-05-04 22:59:22 -0300

    Avance con scopes de la api y añadido window.location.href

--- src/components/Logins.jsx
+++ src/components/Logins.jsx
@@ -7,7 +7,12 @@ function Logins() {
         const redirectURL = "http://localhost:3000/";
         const apiUrl = "http://accounts.spotify.com/authorize"
         const scope = ['user-read-email',
-        'user-read-private']
+        'user-read-private','user-modify-playback-state',
+        'user-read-playback-state',
+        'user-read-currently-playing','user-read-recently-played',
+        'user-read-playback-position',
+        'user-top-read'];
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scope.join(" ")}&response_type=token&show_daialog=true`;
     }
   return (
         <Container>
