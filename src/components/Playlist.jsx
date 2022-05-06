c338ff788e083e4a5f85877246ca3a03401915c2
Ascended300 <eduardofelix014@gmail.com>
2022-05-05 21:45:39 -0300

    Añadido scrollbar sin estilos completos , solo para las playlist, datos copiados momentaneamente(playlist)

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,33 +1,82 @@
-import axios from 'axios';
-import React, {useEffect} from 'react';
-import { reducerCases } from '../utils/Constants';
-import {useStateProvider} from '../utils/StateProvider';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
+import styled from "styled-components";
 
 function Playlist() {
-    const [{token,dispatch}] = useStateProvider();
+    const [{ token, playlists }, dispatch] = useStateProvider();
 
-    useEffect(()=>{
+    useEffect(() => {
         const getPlaylistData = async () => {
-            const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
-                headers: {
-                    Authorization:'Bearer '+ token,
-                    'Content-Type': 'application/json',
-                },
+            const response = await axios.get(
+                "https://api.spotify.com/v1/me/playlists",
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            const { items } = response.data;
+            const playlists = items.map(({ name, id }) => {
+                return { name, id };
             });
-            const {items} = response.data;
-            const playlist = items.map(({name , id}) => {
-                return {name, id};
-            })
-            dispatch({type:reducerCases.SET_PLAYLISTS, playlist})
-            
+            dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
         };
         getPlaylistData();
-        
-    }, [token,dispatch]);
+    }, [token, dispatch]);
 
     return (
-    <div>Playlist</div>
-  )
+        <Container>
+            <ul>
+                {
+                    playlists.map(({ name, id }) => {
+                        return <li key={id}>{name}</li>;
+                    })
+                }
+                {
+                    playlists.map(({ name, id }) => {
+                        return <li key={id}>{name}</li>;
+                    })
+                }
+                {
+                    playlists.map(({ name, id }) => {
+                        return <li key={id}>{name}</li>;
+                    })
+                }
+
+            </ul>
+
+        </Container>
+    );
 }
 
 export default Playlist;
+
+const Container = styled.div`
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 52vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+        display: none;
+        }
+    li {
+      cursor: pointer;
+      display: flex;
+      gap: 1rem;
+      transition: 0.3 ease-in-out;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
